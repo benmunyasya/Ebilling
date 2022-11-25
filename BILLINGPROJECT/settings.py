4ca36e78308f3bc856597b729675ef6fda85d4ff
unknown <benngovi47@gmail.com>
2022-11-25 16:32:20 +0300

    commit 3.4

--- BILLINGPROJECT/settings.py
+++ BILLINGPROJECT/settings.py
@@ -148,7 +148,6 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 COLLECTSTATIC = 0
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import MpesaPayment,MpesaCallBacks,MpesaCalls
+from .models import MpesaPayment,MpesaCallBacks,MpesaCalls,Client,WaterBillingCycle
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(WaterBillingCycle)
 admin.site.register(MpesaPayment)
 admin.site.register(MpesaCalls)
 admin.site.register(MpesaCallBacks)
